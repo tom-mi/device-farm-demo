c6040d9b9db98fc0b7dc514a30da184c8a53b5ed
Thomas Reifenberger <tom-mi@users.noreply.github.com>
2019-02-22 12:26:02 +0100

    Fix pipeline & add script to push sources

---
+++ push_sources.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eu
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <prefix>"
+    exit 1
+fi
+
+PREFIX=$1
+
+if [[ -e sources.zip ]]; then
+    rm sources.zip
+fi
+
+echo "Packaging sources"
+zip --quiet -r sources.zip app gradlew gradle build.gradle gradle.properties pipeline/buildspec.yaml
+
+pipeline_stack_name="${PREFIX}-device-farm-demo-pipeline"
+source_bucket=$(aws cloudformation describe-stacks \
+    --output text \
+    --stack-name ${pipeline_stack_name} \
+    --query 'Stacks[0].Outputs[?OutputKey==`SourceBucket`].OutputValue')
+
+echo "Pushing sources to ${source_bucket}"
+aws s3 cp sources.zip s3://${source_bucket}/sources.zip
